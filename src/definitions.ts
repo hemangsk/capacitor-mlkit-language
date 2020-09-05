711835c9e007ebbdd37973c4c7eccc33c6e0b6a4
Hemang Kumar <hemangsk@gmail.com>
2020-09-06 01:30:42 +0530

    plugin: Remove web.ts
    This plugin is limited to android and iOS until we have
    MLKit for browser.

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,9 +1,7 @@
-declare module '@capacitor/core' {
+declare global {
   interface PluginRegistry {
     MLKitLanguage: MLKitLanguagePlugin;
   }
 }
 
-export interface MLKitLanguagePlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
-}
+export interface MLKitLanguagePlugin {}
