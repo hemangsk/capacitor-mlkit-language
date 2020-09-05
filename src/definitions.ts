d7536892e534ab3d92b49b5423a69bb3ab59141e
Hemang Kumar <hemangsk@gmail.com>
2020-09-06 03:32:29 +0530

    src/mlkit: Add TS interface for Plugin

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,7 +1,33 @@
+import {
+  IdentifiedLanguage,
+  IdentifiedLanguagesResponse,
+  LanguageIdentificationRequest,
+} from './mlkit';
+
 declare global {
   interface PluginRegistry {
-    MLKitLanguage: MLKitLanguagePlugin;
+    MLKit: MLKitPlugin;
   }
 }
 
-export interface MLKitLanguagePlugin {}
+export interface MLKitPlugin {
+  /**
+   * Identify the language of the provided text.
+   *
+   * @param {LanguageIdentificationRequest} options Request object containing text and language identification options.
+   * @returns {Promise<IdentifiedLanguage>} The identified language
+   */
+  NLLIdentifyLanguage(
+    options: LanguageIdentificationRequest,
+  ): Promise<IdentifiedLanguage>;
+
+  /**
+   * Identify the possible languages of the provided text.
+   *
+   * @param {LanguageIdentifierOptions} options Request object containing text and language identification options.
+   * @returns {Promise<IdentifiedLanguagesResponse>}
+   */
+  NLLIdentifyPossibleLanguages(
+    options: LanguageIdentificationRequest,
+  ): Promise<IdentifiedLanguagesResponse>;
+}
---
+++ src/mlkit/language.ts
@@ -0,0 +1,103 @@
+import { Plugins } from '@capacitor/core';
+
+const { MLKit } = Plugins;
+
+/**
+ * A LanguageIdentification client for identifying the language of a piece of text.
+ */
+export class LanguageIdentification {
+  /**
+   * Gets the languageIdentifier class instance.
+   *
+   * @param {LanguageIdentifierOptions} options Options for languageIdentification. Includes `confidenceThreshold`.
+   * @returns {LanguageIdentifier}
+   */
+  public static getClient(
+    options?: LanguageIdentifierOptions,
+  ): LanguageIdentifier {
+    if (options) {
+      return new LanguageIdentifier(options);
+    }
+
+    return new LanguageIdentifier({});
+  }
+}
+
+/**
+ * Contains language identification techniques from the API
+ */
+export class LanguageIdentifier {
+  languageIdentifierOptions: LanguageIdentifierOptions;
+
+  constructor(options: LanguageIdentifierOptions) {
+    this.languageIdentifierOptions = options;
+  }
+
+  /**
+   * Identify the language of the provided text.
+   *
+   * @param {string} text The text whose language needs to be identified.
+   * @returns {Promise<IdentifiedLanguage>} Object having languageCode of the identified language
+   */
+  public identifyLanguage(text: string): Promise<IdentifiedLanguage> {
+    return MLKit.NLLIdentifyLanguage({
+      text,
+      languageIdentifierOptions: this.languageIdentifierOptions,
+    });
+  }
+
+  /**
+   * Identify the possible languages of the provided text.
+   *
+   * @param {string} text The text whose language needs to be identified.
+   * @returns {Promise<IdentifiedLanguage>} Object having list of languageCodes of the possible languages with the confidence of each.
+   */
+  public identifyPossibleLanguages(
+    text: string,
+  ): Promise<IdentifiedLanguagesResponse> {
+    return MLKit.NLLIdentifyPossibleLanguages({
+      text,
+      languageIdentifierOptions: this.languageIdentifierOptions,
+    });
+  }
+}
+
+export interface IdentifiedLanguage {
+  /**
+   * confidence score of the identified language
+   */
+  confidence?: number;
+  /**
+   * 'Two character code' of the identified language
+   */
+  languageCode: string;
+}
+
+export interface IdentifiedLanguagesResponse {
+  /**
+   * List of identified languages.
+   * Contains response of LanguageIdentifier's [[identifyPossibleLanguages]] method.
+   */
+  possibleLanguages: IdentifiedLanguage[];
+}
+
+export interface LanguageIdentifierOptions {
+  /**
+   * The minimum threshold of confidence expected while language Identification.
+   * Check google's docs for official defaults for `confidenceThreshold`.
+   *
+   * https://developers.google.com/android/reference/com/google/mlkit/nl/languageid/LanguageIdentifier#constant-summary
+   */
+  confidenceThreshold?: string;
+}
+
+export interface LanguageIdentificationRequest {
+  /**
+   * Text to be translated
+   */
+  text: string;
+  /**
+   * Options for language identification.
+   */
+  languageIdentifierOptions?: LanguageIdentifierOptions;
+}
