503c309db8f0a218f7e5d8a621753f13b2eb1b41
Hemang Kumar <hemangsk@gmail.com>
2020-09-06 03:29:13 +0530

    ios/Plugin.m: Update with actual PluginMethods
    Related to commit: 5469ebb97d9e808cd13b429ff36687cfe246870e

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -4,5 +4,6 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(MLKitLanguage, "MLKitLanguage",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(NLLIdentifyLanguage, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(NLLIdentifyPossibleLanguages, CAPPluginReturnPromise);
 )
